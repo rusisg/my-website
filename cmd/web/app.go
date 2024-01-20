46d6bab7cd746df3a96deb44d92841ba7b094c90
rusisg <rasul.nulrybekov@gmail.com>
2024-01-20 19:25:45 +0600

    serve handle static files

--- cmd/web/app.go
+++ cmd/web/app.go
@@ -12,6 +12,9 @@ func App() {
 	mux.HandleFunc("/", Home)
 	mux.HandleFunc("/blog", Blog)
 
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Printf("Starting server at %s", port)
 	err := http.ListenAndServe(port, mux)
 	if err != nil {
