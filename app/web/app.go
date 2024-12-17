b1e771adbd5bebb3338abd7622f5dbddccd40ad4
rusisg <rasul.nulrybekov@gmail.com>
2024-12-17 20:54:22 +0100

    main .go file

---
+++ app/web/app.go
@@ -0,0 +1,24 @@
+package web
+
+import (
+	"log"
+	"net/http"
+	"personal-website-template/app/handlers"
+)
+
+const port = ":4000"
+
+func App() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handlers.Home)
+	mux.HandleFunc("/blog", handlers.Blog)
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
+
+	log.Printf("Starting server at %s", port)
+	err := http.ListenAndServe(port, mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
