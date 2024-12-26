11468cade8a65eeec26acb902b7b9f468911e94f
rusisg <rasul.nulrybekov@gmail.com>
2024-12-26 23:58:22 +0100

    change: new note admin page serve

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -11,6 +11,7 @@ func Home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
+	w.WriteHeader(http.StatusOK)
 
 	files := []string{
 		"./assets/html/home.page.gohtml",
@@ -37,6 +38,8 @@ func Note(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
+	w.WriteHeader(http.StatusOK)
+
 	files := []string{
 		"./assets/html/note.page.gohtml",
 		"./assets/html/base.layout.gohtml",
@@ -55,4 +58,28 @@ func Note(w http.ResponseWriter, r *http.Request) {
 	}
 
 	log.Println("note page")
+}
+
+func NoteAdmin(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/note/admin" {
+		http.NotFound(w, r)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	files := []string{
+		"./assets/html/note_admin.page.gohtml",
+		"./assets/html/base.layout.gohtml",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+	log.Println("note admin page")
 }
