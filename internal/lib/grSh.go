8cc32be8d59d8d0781cdf251cbe0b3a4e87185b6
rusisg <rasul.nulrybekov@gmail.com>
2025-01-17 14:11:52 +0100

    new: separate the Graceful Shutdown logic in lib package

---
+++ internal/lib/grSh.go
@@ -0,0 +1,40 @@
+package lib
+
+import (
+	"context"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func GracefulShutdown(srv *http.Server) error {
+	var wait time.Duration
+	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully waits for existing connections to finish - e.g., 15s or 1m")
+	flag.Parse()
+
+	go func() {
+		log.Printf("Starting server at %s", srv.Addr)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("ListenAndServe failed: %s", err)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
+	<-stop
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %s", err)
+	}
+
+	log.Println("Server gracefully stopped")
+	return nil
+}
