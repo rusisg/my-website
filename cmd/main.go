6555c015e3592ee8d87aca19889295879de7a148
rusisg <rasul.nulrybekov@gmail.com>
2025-01-17 14:12:29 +0100

    update: clean main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,13 @@
 package main
 
 import (
-	"context"
-	"flag"
 	"log"
 	"net/http"
-	"os"
-	"os/signal"
 	"personal-website-template/internal/handlers"
-	"time"
+	"personal-website-template/internal/lib"
 )
 
 func main() {
-	var wait time.Duration
-	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully waits for existing connections to finish - e.g., 15s or 1m")
-	flag.Parse()
-
 	mux := http.NewServeMux()
 	handlers.RegisterRoutes(mux)
 
@@ -27,29 +19,12 @@ func main() {
 	mux.Handle("/img/", http.StripPrefix("/img/", imgFileServer))
 
 	srv := &http.Server{
-		Addr:    ":4000",
+		Addr:    ":5000",
 		Handler: mux,
 	}
 
-	go func() {
-		log.Printf("Starting server at %s", srv.Addr)
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("ListenAndServe failed: %s", err)
-		}
-	}()
-
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, os.Interrupt)
-
-	<-stop
-	log.Println("Shutting down server...")
-
-	ctx, cancel := context.WithTimeout(context.Background(), wait)
-	defer cancel()
-
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatalf("Server forced to shutdown: %s", err)
+	err := lib.GracefulShutdown(srv)
+	if err != nil {
+		log.Fatalf("Failed to graceful shutdown: %s", err)
 	}
-
-	log.Println("Server gracefully stopped")
 }
