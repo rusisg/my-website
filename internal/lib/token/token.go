c61c93c92d07df47614ccfb9299acfd96071c44c
rusisg <rasul.nulrybekov@gmail.com>
2025-01-26 16:51:40 +0100

    new: new lib util that generates token

---
+++ internal/lib/token/token.go
@@ -0,0 +1,15 @@
+package token
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"log"
+)
+
+func GenerateToken(length int) string {
+	bytes := make([]byte, length)
+	if _, err := rand.Read(bytes); err != nil {
+		log.Fatalf("Failed to generate token: %v", err)
+	}
+	return base64.URLEncoding.EncodeToString(bytes)
+}
