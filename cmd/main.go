41fdc1a86e6834f94948efde63c69e8dc9d3de43
rusisg <rasul.nulrybekov@gmail.com>
2025-01-30 12:32:41 +0100

    update: instead localhost it is on port and host of service

--- cmd/main.go
+++ cmd/main.go
@@ -3,28 +3,62 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"personal-website-template/internal/handlers"
 	"personal-website-template/internal/lib/graceful_shutdown"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found. Falling back to system environment variables.")
+	}
+
 	mux := http.NewServeMux()
+
+	// Register application routes
 	handlers.RegisterRoutes(mux)
 
+	// Serve static files: CSS, JS
 	fileServer := http.FileServer(http.Dir("./assets/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
 
-	// Static files: Images (including CV file)
+	// Serve image files (including CV)
 	imgFileServer := http.FileServer(http.Dir("./assets/img/"))
 	mux.Handle("/img/", http.StripPrefix("/img/", imgFileServer))
 
+	// Get port and host from environment variables or use defaults
+	port := os.Getenv("ALWAYSDATA_HTTPD_PORT")
+	if port == "" {
+		log.Println("ALWAYSDATA_HTTPD_PORT not set. Falling back to default port 8100.")
+		port = "8100"
+	}
+
+	host := os.Getenv("ALWAYSDATA_HTTPD_IP")
+	if host == "" {
+		log.Println("ALWAYSDATA_HTTPD_IP not set. Falling back to default host 0.0.0.0.")
+		host = "0.0.0.0"
+	}
+
+	// Wrap IPv6 addresses in square brackets
+	if host == "::" || host == "0:0:0:0:0:0:0:1" {
+		host = "[" + host + "]"
+	}
+
+	addr := host + ":" + port
+
+	// Create HTTP server
 	srv := &http.Server{
-		Addr:    ":5000",
+		Addr:    addr,
 		Handler: mux,
 	}
 
-	err := graceful_shutdown.GracefulShutdown(srv)
+	// Handle graceful shutdown
+	err = graceful_shutdown.GracefulShutdown(srv)
 	if err != nil {
-		log.Fatalf("Failed to graceful shutdown: %s", err)
+		log.Fatalf("Failed to gracefully shut down: %s", err)
 	}
 }
